d98aa3d165140d9d3792e45c2c98ddabd1bbef89
Randall <randallgears1998@gmail.com>
2021-05-26 21:42:09 -0600

    Login y Menu administrador

---
+++ ProyectoHoteleroFARS/Entidades/Rol.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    public class Rol
+    {
+        public int TN_Id { set; get; }
+        public string TC_Rol { set; get; }
+    }
+}
