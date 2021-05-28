8ff6b9aeea80a0a1f1c316f9d24e6b8d8ba0f4ab
steven <ste98moragmail.com>
2021-05-28 10:45:45 -0600

    ContactenosView
    VistaContactenos

---
+++ ProyectoHoteleroFARS/ReglasNegocio/ContactoRN.cs
@@ -0,0 +1,36 @@
+﻿using AccesoDatos;
+using Entidades;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReglasNegocio
+{
+    public class ContactoRN
+    {
+        public List<Contactos> getContactosRN()
+        {
+            List<Contactos> contactosList = new List<Contactos>();
+            ContactosAD conAD= new ContactosAD();
+            string respuesta = null;
+            try
+            {
+
+                respuesta = conAD.getContactos();
+                if (respuesta != null)
+                {
+                    contactosList = JsonConvert.DeserializeObject<List<Contactos>>(respuesta);
+                }
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return contactosList;
+        }
+
+    }
+}
