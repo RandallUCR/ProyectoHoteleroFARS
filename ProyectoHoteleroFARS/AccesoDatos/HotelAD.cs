04b55413e9e30d8a446674f90b4a73c1c2220b5e
Randall <randallgears1998@gmail.com>
2021-06-25 15:29:10 -0600

    Auto stash before merge of "main" and "origin/main"

--- ProyectoHoteleroFARS/AccesoDatos/HotelAD.cs
+++ ProyectoHoteleroFARS/AccesoDatos/HotelAD.cs
@@ -44,5 +44,31 @@ namespace AccesoDatos
             return data;
         }
 
+        public int registrarSugerencia(string sug)
+        {
+            int result = 2;
+
+            try
+            {
+                SqlDataReader dr = consultar($"EXEC sp_sugerencia '{sug}'");
+                if (dr != null)
+                {
+                    dr.Read();
+
+                    result = int.Parse(dr[0].ToString());
+
+                }
+                else
+                {
+                    result = 2;
+                }
+            }
+            catch (SqlException e)
+            {
+                result = 2;
+            }
+            return result;
+        }
+
     }
 }
