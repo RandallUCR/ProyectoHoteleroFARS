cff23402883270da75e7acea4258cd53b7118c12
Randall <randallgears1998@gmail.com>
2021-06-30 21:21:19 -0600

    Front del CRUD tipos y habitaciones

---
+++ ProyectoHoteleroFARS/ProyectoHoteleroFARS/wwwroot/js/adminhabitaciones.js
@@ -0,0 +1,27 @@
+﻿function modalInsTipo() {
+    $('#modalInsertTipo').modal("show");
+}
+
+function modalInsHabi() {
+    $('#modalInsertHabi').modal("show");
+}
+
+function eliminarTipo(id) {
+    alert(id);
+}
+
+function eliminarHabi(id) {
+    alert(id);
+}
+
+function modalInsHabi(id,num,estado){
+    $('#idhabi').val(id);
+    $('#nume').val(num);
+    if (estado == 0) {
+        $('#estadoe').attr('checked', false);
+    } else {
+        $('#estadoe').attr('checked', true);
+    }
+
+    $('#modalEditHabi').modal("show");
+}
