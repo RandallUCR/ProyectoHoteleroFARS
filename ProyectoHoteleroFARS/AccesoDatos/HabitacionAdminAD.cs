7809825e678cb451b567996699aeb506152e87e8
Randall <randallgears1998@gmail.com>
2021-07-01 17:43:39 -0600

    Backend habitaciones parte 1

--- ProyectoHoteleroFARS/AccesoDatos/HabitacionAdminAD.cs
+++ ProyectoHoteleroFARS/AccesoDatos/HabitacionAdminAD.cs
@@ -24,18 +24,109 @@ namespace AccesoDatos
 
                         h.TN_Id = int.Parse(dr[0].ToString());
                         h.TN_Numero = int.Parse(dr[1].ToString());
-                        h.TB_Estado = int.Parse(dr[2].ToString());
+                        h.TB_Estado = bool.Parse(dr[2].ToString());
+
+                        TipoHabitacion t = new TipoHabitacion();
+                        t.TN_Id = int.Parse(dr[3].ToString());
+                        t.TC_Nombre = dr[4].ToString();
+
+                        h.tipo = t;
 
                         list.Add(h);
                     }
-                                       
+
+
                 }
             }
             catch (SqlException e) {
-                list = null;
+                //list = null;               
+            }
+            return JsonConvert.SerializeObject(list);
+        }
+
+        public int insertarHabitacion(Habitacion h)
+        {
+            int result = 3;
+            int aux = 0;
+            if (h.TB_Estado)
+            {
+                aux = 1;
             }
 
-            return JsonConvert.SerializeObject(list);
+            try
+            {
+                SqlDataReader dr = consultar($"EXEC sp_ins_habitacion {h.TN_Numero}, {aux}, {h.tipo.TN_Id}");
+                if (dr != null)
+                {
+                    dr.Read();
+
+                    result = int.Parse(dr[0].ToString());
+                }
+                else
+                {
+                    result = 3;
+                }
+            }
+            catch (SqlException e)
+            {
+                result = 3;
+            }
+            return result;
+        }
+
+        public int modificarHabitacion(Habitacion h)
+        {
+            int result = 3;
+            int aux = 0;
+            if (h.TB_Estado)
+            {
+                aux = 1;
+            }
+
+            try
+            {
+                SqlDataReader dr = consultar($"EXEC sp_edit_habitacion {h.TN_Numero}, {aux}, {h.TN_Id}");
+                if (dr != null)
+                {
+                    dr.Read();
+
+                    result = int.Parse(dr[0].ToString());
+                }
+                else
+                {
+                    result = 3;
+                }
+            }
+            catch (SqlException e)
+            {
+                result = 3;
+            }
+            return result;
+        }
+
+        public int eliminarHabitacion(int id)
+        {
+            int result = 3;
+
+            try
+            {
+                SqlDataReader dr = consultar($"EXEC sp_del_habitacion {id}");
+                if (dr != null)
+                {
+                    dr.Read();
+
+                    result = int.Parse(dr[0].ToString());
+                }
+                else
+                {
+                    result = 3;
+                }
+            }
+            catch (SqlException e)
+            {
+                result = 3;
+            }
+            return result;
         }
     }
 }
--- ProyectoHoteleroFARS/ReglasNegocio/HabitacionAdminRN.cs
+++ ProyectoHoteleroFARS/ReglasNegocio/HabitacionAdminRN.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AccesoDatos;
+using Entidades;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +9,82 @@ namespace ReglasNegocio
 {
     public class HabitacionAdminRN
     {
+        public List<Habitacion> getHabitacionesByTipo(int id)
+        {
+            List<Habitacion> list = new List<Habitacion>();
+            HabitacionAdminAD ad = new HabitacionAdminAD();
+
+            string respuesta = null;
+
+            try
+            {
+                respuesta = ad.getHabitacionesByTipo(id);               
+                if (respuesta != null)
+                {
+                    list = JsonConvert.DeserializeObject<List<Habitacion>>(respuesta);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("RN");
+            }
+
+            return list;
+        }
+
+        public int insertarHabitacion(int num, bool estado, int tipo)
+        {
+            HabitacionAdminAD rad = new HabitacionAdminAD();
+            int result = 3;
+            try
+            {
+                result = rad.insertarHabitacion(new Habitacion() { TN_Numero = num, TB_Estado = estado, tipo = new TipoHabitacion() { TN_Id = tipo} });
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                result = 3;
+            }
+
+            return result;
+        }
+
+        public int modificarHabitacion(int num, bool estado, int id)
+        {
+            HabitacionAdminAD rad = new HabitacionAdminAD();
+            int result = 3;
+            try
+            {
+                result = rad.modificarHabitacion(new Habitacion() { TN_Id = id, TN_Numero = num, TB_Estado = estado});
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                result = 3;
+            }
+
+            return result;
+        }
+
+        public int eliminarHabitacion(int id)
+        {
+            HabitacionAdminAD rad = new HabitacionAdminAD();
+            int result = 3;
+            try
+            {
+                result = rad.eliminarHabitacion(id);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                result = 3;
+            }
+
+            return result;
+        }
     }
 }
