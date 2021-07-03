a148e1375df85a8199050eb908fd0b1855dbf8db
Randall <randallgears1998@gmail.com>
2021-07-02 21:50:52 -0600

    final habitaciones

--- ProyectoHoteleroFARS/ReglasNegocio/TipoHabitacionAdminRN.cs
+++ ProyectoHoteleroFARS/ReglasNegocio/TipoHabitacionAdminRN.cs
@@ -45,5 +45,30 @@ namespace ReglasNegocio
 
             return result;
         }
+
+        public int modificarTiposHabitaciones(int id, string nombre, string desc, int precio, string base64, string formato)
+        {
+            TipoHabitacionAdminAD tad = new TipoHabitacionAdminAD();
+            int result = -3;
+            try
+            {
+                result = tad.modificarTiposHabitaciones(new TipoHabitacion()
+                {
+                    TN_Id = id,
+                    TC_Nombre = nombre,
+                    TC_Descripcion = desc,
+                    TN_Precio = precio,
+                    galeria = new Galeria() { TV_Archivo = base64, TC_Formato = formato }
+                });
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                result = -3;
+            }
+
+            return result;
+        }
     }
 }
