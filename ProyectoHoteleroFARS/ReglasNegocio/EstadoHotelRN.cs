cb194f740efa7e491ff2f69be531691d99d90ea3
Faubricio Chaves <faubricio98@gmail.com>
2021-07-16 14:27:17 -0600

    Estado del hotel para el día de hoy, sin reporte

---
+++ ProyectoHoteleroFARS/ReglasNegocio/EstadoHotelRN.cs
@@ -0,0 +1,17 @@
+﻿using AccesoDatos;
+using Entidades;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReglasNegocio
+{
+    public class EstadoHotelRN{
+
+        public List<Habitacion> getEstadoHotelHoyRN() {
+            return new EstadoHotelAD().getEstadoHotelHoyAD();
+        }
+
+    }
+}
