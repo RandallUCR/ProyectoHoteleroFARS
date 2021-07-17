1c9319b27dd6d7cfb43138396ce34970f30ea05d
Randall <randallgears1998@gmail.com>
2021-07-16 23:32:12 -0600

    CRUD administradores

---
+++ ProyectoHoteleroFARS/ProyectoHoteleroFARS/wwwroot/js/admin.js
@@ -0,0 +1,141 @@
+﻿function modalEditAdmin(id, user, pass,rol) {
+    $('#idAdmin').val(id);
+    $('#usere').val(user);
+    $('#passe').val(pass);
+    $("#role").val(rol).change();
+
+    $('#modalEditAdmin').modal("show");
+}
+
+function modalInsAdmin() {
+    $('#modalInsAdmin').modal("show");
+}
+
+$('#adminInsForm').submit(function (e) {
+    e.preventDefault();
+    var form = $(this);
+    var url = form.attr('action');
+    $.ajax({
+        type: 'post',
+        url: url,
+        data: form.serialize(),
+        success: function (response) {
+            if (response.success == true) {
+                if (response.inserted == true) {
+                    window.location.href = response.url;
+                } else {
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'error',
+                        title: 'Ya hay un administrador con ese usuario.',
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            } else {
+                Swal.fire({
+                    position: 'center',
+                    icon: 'error',
+                    title: 'Error en el sistema.',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+        },
+        error: function (response) {
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: 'Error en el sistema.',
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
+    });
+});
+
+$('#adminModForm').submit(function (e) {
+    e.preventDefault();
+    var form = $(this);
+    var url = form.attr('action');
+    $.ajax({
+        type: 'post',
+        url: url,
+        data: form.serialize(),
+        success: function (response) {
+            if (response.success == true) {
+                if (response.inserted == true) {
+                    window.location.href = response.url;
+                } else {
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'error',
+                        title: 'Ya hay un administrador con ese usuario.',
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            } else {
+                Swal.fire({
+                    position: 'center',
+                    icon: 'error',
+                    title: 'Error en el sistema.',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+        },
+        error: function (response) {
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: 'Error en el sistema.',
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
+    });
+});
+
+function EliminarAdmin(id) {
+    Swal.fire({
+        title: '¿Seguro?',
+        text: 'No se podrán revertir los cambios',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si, eliminar',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.value) {
+            // Proceso de eliminacion de datos
+            var data = { id: id }
+            $.ajax({
+                url: '/Administrador/Eliminar',
+                type: 'POST',
+                data: data,
+                success: function (response) {
+                    if (response.success == true) {
+                            window.location.href = response.url;
+                    } else {
+                        Swal.fire({
+                            position: 'center',
+                            icon: 'error',
+                            title: 'Error al eliminar.',
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                    }
+                },
+                error: function (response) {
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'error',
+                        title: 'Error en el sistema.',
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            });
+        }
+    })
+}
--- ProyectoHoteleroFARS/ReglasNegocio/AdministradorRN.cs
+++ ProyectoHoteleroFARS/ReglasNegocio/AdministradorRN.cs
@@ -31,5 +31,81 @@ namespace ReglasNegocio
             }
             return a;
         }
+
+        public List<Administrador> lista_admins()
+        {
+            AdministradorAD rad = new AdministradorAD();
+            List<Administrador> lista = new List<Administrador>();
+
+            string respuesta = null;
+            try
+            {
+                respuesta = rad.lista_admins();
+                if (respuesta != null)
+                {
+                    lista = JsonConvert.DeserializeObject<List<Administrador>>(respuesta);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return lista;
+        }
+
+        public int insertarAdmin(string usuario, string contrasena, int rol)
+        {
+            AdministradorAD rad = new AdministradorAD();
+            int result = 3;
+            try
+            {
+                result = rad.insertarAdmin(new Administrador() { TC_Usuario = usuario, TC_Contrasena = contrasena, rol = new Rol() { TN_Id = rol} });
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                result = 3;
+            }
+
+            return result;
+        }
+
+        public int modificarAdmin(int id, string usuario, string contrasena, int rol)
+        {
+            AdministradorAD rad = new AdministradorAD();
+            int result = 3;
+            try
+            {
+                result = rad.modificarAdmin(new Administrador() { TN_Id = id, TC_Usuario = usuario, TC_Contrasena = contrasena, rol = new Rol() { TN_Id = rol } });
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                result = 3;
+            }
+
+            return result;
+        }
+
+        public int eliminarAdmin(int id)
+        {
+            AdministradorAD rad = new AdministradorAD();
+            int result = 2;
+            try
+            {
+                result = rad.eliminarAdmin(id);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                result = 2;
+            }
+
+            return result;
+        }
     }
 }
